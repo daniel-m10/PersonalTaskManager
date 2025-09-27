bcaceb98ac4cdff5530932958ccb346dc62344a0
daniel-m10 <daniel.montecinos.pinto@gmail.com>
2025-09-27 16:13:20 -0400

    Implement ITaskRepository interface and add corresponding unit tests

---
+++ src/TaskManager.Core/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Core.Entities;
+
+namespace TaskManager.Core.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<TaskItem> Create(TaskItem taskItem);
+        Task<IEnumerable<TaskItem>> GetAll();
+        Task<TaskItem> GetById(Guid id);
+        Task<TaskItem> Update(TaskItem taskItem);
+        Task<bool> Delete(Guid id);
+    }
+}
